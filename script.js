b3f509c82ca7a8ac982f38194ae06fa098e840f6
jamieYeung <jamieyyw@gmail.com>
2019-06-15 16:30:55 -0400

    added way more styles

--- script.js
+++ script.js
@@ -1,19 +1,10 @@
 
-// for toggling through slides
-const slideIndex = 1;
-showProject(slideIndex);
-
-function plusDivs(n) {
-  showProject(slideIndex += n);
-}
-
-function showProject(n) {
-  const i;
-  const x = $(".project");
-  if (n > x.length) { slideIndex = 1 }
-  if (n < 1) { slideIndex = x.length }
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  x[slideIndex - 1].style.display = "block";
-}
+$(function () {
+  $('.projectsContainerList').flickity({
+  // options
+  cellAlign: 'left',
+  wrapAround: true,
+  imagesLoaded: true,
+  contain: true
+  });
+});
