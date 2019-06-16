23e0e1c25876b73849ea3cc9e804bbb50d22cce5
jamieYeung <jamieyyw@gmail.com>
2019-06-16 15:50:13 -0400

    responsive and new header pic, still need to update mobile abit more

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
 $(function () {
   $('.projectsContainerList').flickity({
-  // options
-  cellAlign: 'left',
-  wrapAround: true,
-  imagesLoaded: true,
-  contain: true
-  });
+    // options
+    cellAlign: 'left',
+    wrapAround: true,
+    imagesLoaded: true,
+    contain: true
+    });
   $('.hamburger').click(function () {
       $('.menu').toggleClass('show');
     });
