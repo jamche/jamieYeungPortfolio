7c6ad85283dab7aaeff39812340de63bbf143205
jamieYeung <jamieyyw@gmail.com>
2019-06-18 23:58:07 -0400

    removed flickity, layout to show projects instead

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 
 $(function () {
   // run flickity
-  $('.projectsContainerList').flickity({
-    // options
-    cellAlign: 'left',
-    wrapAround: true,
-    imagesLoaded: true,
-    contain: true
-    });
+  // $('.projectsContainerList').flickity({
+  //   // options
+  //   cellAlign: 'left',
+  //   wrapAround: true,
+  //   imagesLoaded: true,
+  //   contain: true
+  //   });
     // hamburger
   $('.hamburger').click(function () {
     $('.menu').addClass('show');
     $('.menu').addClass('slide-in-left');
     $('.menu').removeClass('slide-out-left');
+    $('.hamburger').css('visibility','hidden');
     });
     $('.exitMenu').click(function () {
       $('.menu').toggleClass('slide-out-left');
       $('.menu').removeClass('slide-in-left');  
+      $('.hamburger').css('visibility', 'initial');
     });
   $('.clickClose').click(function(){
     $('.menu').removeClass('show');
